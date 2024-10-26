6cc34d5f64d7551784d2871017a2a57316445c22
soorajkabeer <soorajkabeer@gmail.com>
2024-10-27 00:09:30 +0100

    dark/light mode added

--- src/app/components/NavBar.js
+++ src/app/components/NavBar.js
@@ -1,12 +1,22 @@
 "use client";
 import React from "react";
 import CustomNavLink from "./CustomNavLink";
+import useThemeSwitcher from "./hooks/useThemeSwitcher";
+import { MoonIcon, SunIcon } from "./Icon";
 
 const NavBar = () => {
+  const [mode, setMode] = useThemeSwitcher();
+
   return (
     <header>
       <nav className="font-primary flex justify-between items-center py-6 px-11 text-3xl text-theme-black dark:text-theme-white">
-        <p>Day/Night Icon</p>
+        <button onClick={() => setMode(mode === "light" ? "dark" : "light")}>
+          {mode === "dark" ? (
+            <MoonIcon className="text-theme-white" />
+          ) : (
+            <SunIcon className="text-theme-black" />
+          )}
+        </button>
         <nav className="flex">
           <CustomNavLink href="/" title="Home" className="ml-10" />
           <CustomNavLink className="ml-10" href="/about" title="About" />
