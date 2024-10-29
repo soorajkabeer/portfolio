6d53351022438ebaedcf45c04a14471901cdbb4a
soorajkabeer <soorajkabeer@gmail.com>
2024-10-29 21:53:09 +0000

    add comments to the MousePointer componenent

--- src/app/components/MousePointer.js
+++ src/app/components/MousePointer.js
@@ -3,28 +3,32 @@ import React, { useEffect, useState } from "react";
 import { useSpring, motion, useMotionValue } from "framer-motion";
 
 const MousePointer = () => {
+  //using the useMotionValue hook to track cursor point
   const cursorX = useMotionValue(-100);
   const cursorY = useMotionValue(-100);
+  //a state to identify if hover on an element
   const [isHovered, setIsHovered] = useState(false);
 
+  //using useEffects to fetch the changes and read cursor points
   useEffect(() => {
     const moveCursor = (e) => {
+      //subscrating 16 from cursor point values to make the point center
       cursorX.set(e.clientX - 16);
       cursorY.set(e.clientY - 16);
     };
-
+    //to handle mouse enter funtion to read mouse enter in a, p, headings and button
     const handleMouseEnter = (e) => {
       if (e.target.matches("a, p, h1, h2, h3, h4, h5, button")) {
         setIsHovered(true);
       }
     };
-
+    //to handle mouse exit for the same elements as above
     const handleMouseLeave = (e) => {
       if (e.target.matches("a, p, h1, h2, h3, h4, h5, button")) {
         setIsHovered(false);
       }
     };
-
+    //adding event listerner for movemove, mouseenter and mouseleave
     window.addEventListener("mousemove", moveCursor);
     document.body.addEventListener("mouseenter", handleMouseEnter, true);
     document.body.addEventListener("mouseleave", handleMouseLeave, true);
@@ -36,22 +40,25 @@ const MousePointer = () => {
     };
   }, [cursorX, cursorY]);
 
+  //using useSpring from framer motion to handle a sprint effect
   const springConfig = { damping: 25, stiffness: 700 };
   const cursorXSpring = useSpring(cursorX, springConfig);
   const cursorYSpring = useSpring(cursorY, springConfig);
 
   return (
     <motion.div
-      className="fixed left-0 top-0 rounded-full mix-blend-difference z-50 bg-theme-white pointer-events-none"
+      className={`fixed left-0 top-0 ${
+        isHovered ? "h-8 w-8 " : "h-4 w-4 "
+      } rounded-full mix-blend-difference z-50 bg-theme-white pointer-events-none transition-[height,width] duration-300 ease-in-out`}
       style={{
-        width: isHovered ? 16 : 8,
-        height: isHovered ? 16 : 8,
+        // width: isHovered ? 16 : 8,
+        // height: isHovered ? 16 : 8,
         translateX: cursorXSpring,
         translateY: cursorYSpring,
       }}
-      animate={{
-        scale: isHovered ? 1.5 : 1,
-      }}
+      // animate={{
+      //   scale: isHovered ? 1.5 : 1,
+      // }}
       transition={{
         scale: { duration: 0.2, ease: "easeInOut" }, // Apply easeInOut for smooth entry and exit
         width: { duration: 0.2, ease: "easeInOut" },
